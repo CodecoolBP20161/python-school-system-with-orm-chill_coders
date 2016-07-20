9087a028913533e847091f84789379df64d5603f
Leviathan11 <gubik.levente@gmail.com>
2016-07-20 14:32:11 +0200

    can generate applicant code

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from models import *
-
+import random
 # Write here your console application
+
+
+def app_code_generate(applicants):
+    chars = 'ABCDEFGHIJKLMNOPQRSTVWXYZ123456789'
+    done = False
+    while not done:
+        code = ''
+        for i in range(0, 6):
+            code += chars[random.randrange(0, len(chars))]
+        done = True
+        for applicant in applicants:
+            if code == applicant.app_code:
+                done = False
+    return code
