d2bcfb9aec668f10fc5f9a3bd456165a446b2d2f
turbek <turbek93@gmail.com>
2016-07-20 15:28:18 +0200

    generate app_code and adding app code to applicant implemented

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from models import *
 import random
 # Write here your console application
 
+app_code_list = [applicant.app_code for applicant in Applicant.select()]
 
-def app_code_generate(applicants):
+def app_code_generate(appcodelist):
     chars = 'ABCDEFGHIJKLMNOPQRSTVWXYZ123456789'
     done = False
     while not done:
@@ -11,7 +12,17 @@ def app_code_generate(applicants):
         for i in range(0, 6):
             code += chars[random.randrange(0, len(chars))]
         done = True
-        for applicant in applicants:
-            if code == applicant.app_code:
-                done = False
+        if code in appcodelist:
+            done = False
+    appcodelist.append(code)
     return code
+
+def add_app_code():
+
+    for applicant in Applicant.select():
+        if applicant.app_code is None:
+            applicant.app_code = app_code_generate(app_code_list)
+            applicant.save()
+            print(applicant.first_name + " " + applicant.last_name + " received a new application code: " + applicant.app_code)
+
+add_app_code()
