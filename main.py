25a4cfbd7fa83ad05704957e85aead4f060df10f
dsztanko <szt.dori@gmail.com>
2016-07-20 16:21:14 +0200

    Specifies show_interview_location function

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from models import *
 import random
-# Write here your console application
+
 
 app_code_list = [applicant.app_code for applicant in Applicant.select()]
 
 def app_code_generate(appcodelist):
+    """Randomly generates unique application codes."""
     chars = 'ABCDEFGHIJKLMNOPQRSTVWXYZ123456789'
     done = False
     while not done:
@@ -14,15 +15,32 @@ def app_code_generate(appcodelist):
         done = True
         if code in appcodelist:
             done = False
-    appcodelist.append(code)
+    app_code_list.append(code)
     return code
 
 def add_app_code():
-
+    """Adds previously generated application codes if it is necessary."""
     for applicant in Applicant.select():
         if applicant.app_code is None:
             applicant.app_code = app_code_generate(app_code_list)
             applicant.save()
             print(applicant.first_name + " " + applicant.last_name + " received a new application code: " + applicant.app_code)
 
-add_app_code()
+def show_closest_school(*argv):
+    """Prints specific (app_code) or all the applicants and their interview locations."""
+    if len(argv) >= 1:
+        try:
+            spec_applicant = Applicant.get(Applicant.app_code == argv)
+            print('According to the given application code, {0} {1}\'s interview location is: {2} '.format(
+                spec_applicant.first_name,
+                spec_applicant.last_name,
+                spec_applicant.location.loc_school)
+            )
+        except ValueError:
+            print('Application code is not found.')
+    else:
+        for applicant in Applicant.select():
+            print('{0} {1}\'s interview location is: {2} '.format(applicant.first_name,
+                                                              applicant.last_name,
+                                                              applicant.location.loc_school)
+              )
