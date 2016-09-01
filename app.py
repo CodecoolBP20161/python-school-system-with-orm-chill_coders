3740d3902585eca876bc4d83015327fe66b1038b
Leviathan11 <gubik.levente@gmail.com>
2016-09-01 14:28:55 +0200

    minor pep8 fixes

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def _db_connect():
     db.connect()
 
+
 # This hook ensures that the connection is closed when we've finished
 # processing the request.
 @app.teardown_request
@@ -18,26 +19,29 @@ def _db_close(exc):
     if not db.is_closed():
         db.close()
 
+
 @app.route('/')
 def index():
-    return 'Nothing to see here, really.. NOT YET!'
+    return 'Job is done! Check your database!'
 
 
+# Displaying registration form
 @app.route('/registration', methods=['GET', 'POST'])
 def reg_confirmation():
-    """Displays and edits registration form"""
     cities = City.select()
     if request.method == "POST":
         b = (request.form['first_name'], request.form['last_name'], request.form['e_mail'], request.form['location'])
         a = Check.checker(b[0], b[1], b[2])
-        if a[:] == [True, True, True]:  # GOOD FORM, CREATES NEW FORM
-            Applicant.create(first_name=request.form['first_name'], last_name=request.form['last_name'], location=request.form['location'],
+        if a[:] == [True, True, True]:
+            Applicant.create(first_name=request.form['first_name'],
+                             last_name=request.form['last_name'],
+                             location=request.form['location'],
                              email=request.form['e_mail'])
             return redirect('/')
-        else:  # ERROR IN CHECKER, RETURNS REG.FORM TEMPLATE WITH ERRORS
-            return render_template('registration_form.html', cities=cities, error=True, valid=a, words=b)
-    else:  # GET
-        return render_template('registration_form.html', cities=cities, error=False)
+        else:
+            return render_template('Registration_form.html', cities=cities, error=True, valid=a, words=b)
+    else:
+        return render_template('Registration_form.html', cities=cities, error=False)
 
 if __name__ == '__main__':
     app.run()
